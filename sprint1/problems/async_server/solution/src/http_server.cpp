1e4bb46710287b87493f8be3f8c5707853d55f9f
Alexey <altopchiev@yandex.ru>
2024-09-27 15:11:39 +0300

    async_server #1

---
+++ sprint1/problems/async_server/solution/src/http_server.cpp
@@ -0,0 +1,17 @@
+#include "http_server.h"
+
+
+namespace http_server {
+
+void SessionBase::Run() {
+    // Вызываем метод Read, используя executor объекта stream_.
+    // Таким образом вся работа со stream_ будет выполняться, используя его executor
+    net::dispatch(stream_.get_executor(),
+                  beast::bind_front_handler(&SessionBase::Read, GetSharedThis()));
+}
+
+void ReportError(beast::error_code ec, std::string_view what) {
+    using namespace std::literals;
+    std::cerr << what << ": "sv << ec.message() << std::endl;
+}
+}  // namespace http_server
