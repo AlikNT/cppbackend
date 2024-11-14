43d91a27094d915c3355643e797420ca5c7b8c0c
Alexey Topchiev <altopchiev@yandex.ru>
2024-11-14 20:57:58 +0300

    sprint3/before review

--- sprint3/problems/scores/solution/src/extra_data.cpp
+++
@@ -1,9 +0,0 @@
-#include "extra_data.h"
-
-const json::array & extra_data::ExtraDataStorage::GetLootTypes() const {
-    return loot_types_;
-}
-
-void extra_data::ExtraDataStorage::AddLootTypesJson(json::array loot_types) {
-    loot_types_ = std::move(loot_types);
-}
